dddba16acc7be221961002f37eb8ba28bba8effe
mr-mukherjee03 <mukherjee.inbox@gmail.com>
2025-07-25 18:12:41 +0530

    Updated package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mr-mukherjee03.github.io/', // ← your GitHub repo name here
+  base: '/', // ← your GitHub repo name here
    css:{
     modules:{
       localsConvention:"camelCase",
