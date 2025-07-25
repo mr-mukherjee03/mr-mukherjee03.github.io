c54ecf38564d4d10b8d8ecf73b3376c739849acc
mr-mukherjee03 <mukherjee.inbox@gmail.com>
2025-07-25 20:19:15 +0530

    Improved responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,9 +8,8 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1><CodeEditorSimulator /></h1>
-            <p className={styles.description}>I'm a ML enthusiast with five years of experience! Reach out me!
-            </p>
-            
+            <br></br>
+            <p className={styles.description}>Final-year engineer exploring <strong>Backend and Deep Learning</strong> building scalable systems and intelligent vision pipelines.</p>
             <a className={styles.contactBtn} href="mailto:mukherjee.inbox@gmail.com">Contact Me</a>
         </div>
         <img className={styles.profileImg} src={getImageUrl("hero/profile.png")} alt="Hero Image of me" />
